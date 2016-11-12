11191164da30dfc4239330dc9a894d86929ac584
Elijah Manor <elijah.manor@gmail.com>
2016-11-12 12:46:40 -0800

    Working condition

--- src/index.js
+++ src/index.js
@@ -2,26 +2,13 @@
 
 import spawn from "cross-spawn";
 import os from "os";
-import  { exec } from "child_process";
-
-function isWindows() {
-    return os.platform() === "win32";
-}
-
-console.log( process.argv.slice( 2 ) );
-console.log( "isWindows", isWindows() );
+import { exec } from "child_process";
 
 function normalize( args, isWindows ) {
     return args.map( arg => {
         Object.keys( process.env ).forEach( key => {
-            const hasKey = arg.indexOf( key );
-            if ( hasKey ) {
-                if ( isWindows ) {
-                    arg.replace( `$${ key }`, process.env[ key ] );
-                } else {
-                    arg.replace( `%${ key }%`, process.env[ key ] );
-                }
-            }
+            const regex = new RegExp( `\\$${ key }|%${ key }%`, "i" );
+            arg = arg.replace( regex, process.env[ key ] );
         } );
         return arg;
     } )
@@ -29,8 +16,7 @@ function normalize( args, isWindows ) {
 
 let args = process.argv.slice( 2 );
 if ( args.length === 1 ) {
-    const [ command ] = normalize( args, isWindows() );
-    console.log( "exec", command );
+    const [ command ] = normalize( args );
     exec( command, ( error, stdout, stderr ) => {
         if ( error ) {
             console.error( `exec error: ${ error }` );
@@ -40,8 +26,7 @@ if ( args.length === 1 ) {
         process.stderr.write( stderr );
     });
 } else {
-    args = normalize( args, isWindows() );
+    args = normalize( args );
     const command = args.shift();
-    console.log( "spawn", command, args );
     const proc = spawn( command, args, { stdio: "inherit" } );
 }
