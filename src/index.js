ab7366cb0fafb9a2f58df6725063e53ef721a14c
Gabriel <gabriel.meier@cistec.com>
2017-08-16 08:46:51 +0200

    Fixed a bug, where Windows variable which werent set, return the name as a string

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import spawn from "cross-spawn";
 import os from "os";
 import { exec } from "child_process";
 import exit from 'exit';
+const isWindows = (process.platform.substr(0, 3) === "win");
 
-function normalize( args, isWindows ) {
+function normalize( args ) {
+    const isWindows = (process.platform.substr(0, 3) === "win");
     return args.map( arg => {
-        Object.keys( process.env ).forEach( key => {
-            const regex = new RegExp( `\\$${ key }|%${ key }%`, "i" );
-            arg = arg.replace( regex, process.env[ key ] );
-        } );
+        const regex = new RegExp( `\\$(\\w+)|%(\\w+)%`, "i" );
+        const key = (arg.match(regex) !== null) ? arg.match(regex)[1] : undefined;
+        const value = (isWindows && process.env[key] === undefined) ? '' : process.env[key];
+        arg = arg.replace(regex, value);
         return arg;
     } )
 }
